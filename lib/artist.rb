bce4422436b1e55ea2889d8604e468d4fc06387a
Andrea T <tostonar@gmail.com>
2019-08-28 15:17:35 -0400

    Done again

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+    attr_accessor :name
+
+    @@song_count = 0
+    def initialize(name)
+        @name = name
+        @songs = []
+        
+    end
+
+    def songs
+        @songs
+    end
+
+    def add_song(song)
+        @songs.push(song)
+        @@song_count += 1
+        song.artist = self
+    end
+
+    def add_song_by_name(song_name)
+        song = Song.new(song_name)
+        add_song(song)
+    end
+
+    def self.song_count
+        @@song_count
+    end
+end
